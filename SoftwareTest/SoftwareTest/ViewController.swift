82e969f730df232179ef18c4e8c11c842fcac28c
Arthur Martins Saraiva <85759854+Abrririto@users.noreply.github.com>
2023-04-08 15:49:43 -0300

    Adicionando o projeto que será de teste de software

---
+++ SoftwareTest/SoftwareTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoftwareTest
+//
+//  Created by Arthur Martins Saraiva on 08/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
